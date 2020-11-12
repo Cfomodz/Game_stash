01c8ac5c0bb9d179d4bb4578aecba2d7e45f4fe6
blbaileyiii <blbaileyiii.naa@gmail.com>
2020-11-12 07:57:15 -0600

    Adding DataHolder

---
+++ app/src/main/java/com/example/game_stash/DataHolder.java
@@ -0,0 +1,7 @@
+package com.example.game_stash;
+
+public class DataHolder {
+    private static String data;
+    public static String getData() {return data;}
+    public static void setData(String data) {DataHolder.data = data;}
+}
