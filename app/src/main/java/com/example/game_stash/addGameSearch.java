09a35b88b6b7d68f06277b9912904b9f0a7e8f25
David <david.ashby.lds@gmail.com>
2020-11-11 20:51:49 -0700

    added all activities

---
+++ app/src/main/java/com/example/game_stash/addGameSearch.java
@@ -0,0 +1,14 @@
+package com.example.game_stash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class addGameSearch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_game_search);
+    }
+}
---
+++ app/src/main/java/com/example/game_stash/gameEditor.java
@@ -0,0 +1,14 @@
+package com.example.game_stash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class gameEditor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_editor);
+    }
+}
