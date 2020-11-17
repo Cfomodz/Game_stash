8138386e0cffdd0b2188f45c4176a63c708af1f0
David <david.ashby.lds@gmail.com>
2020-11-17 12:49:48 -0700

    Adding design and button functions

--- app/src/main/java/com/example/game_stash/VAddGameManually.java
+++ app/src/main/java/com/example/game_stash/VAddGameManually.java
@@ -3,8 +3,10 @@ package com.example.game_stash;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toolbar;
 
 public class VAddGameManually extends AppCompatActivity {
@@ -16,4 +18,14 @@ public class VAddGameManually extends AppCompatActivity {
         setContentView(R.layout.activity_add_game_manually);
 
     }
+
+    public void addGame(View view){
+
+        //actually add the game to our JSON via game object,
+        //which we need to create from users' input
+
+        Intent intent = new Intent(this, VUsersListOfGames.class);
+        startActivity(intent);
+    }
+
 }
