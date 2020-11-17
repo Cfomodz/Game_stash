6a5a57c8d39593c8312bc5b5d3b8bedca07f3f09
blbaileyiii <blbaileyiii.naa@gmail.com>
2020-11-17 12:34:04 -0800

    Updating MDataHolder with boolean "field edited" members etc.

--- app/src/main/java/com/example/game_stash/MDataHolder.java
+++ app/src/main/java/com/example/game_stash/MDataHolder.java
@@ -72,5 +72,5 @@ public class MDataHolder {
     public static void setHasBeenEditeduserGameList() {
         MDataHolder.hasBeenEditeduserGameList = false;
     }
-    //TESTING SOMETHING...
+    //TESTING SOMETHING...ELSE
 }
