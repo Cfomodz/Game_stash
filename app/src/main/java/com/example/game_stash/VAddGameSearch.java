d5b265b4e993ea7dd7f1a6647dc654ad5fe97ca2
David <david.ashby.lds@gmail.com>
2020-11-17 19:26:11 -0700

    Adding design and button functions

--- app/src/main/java/com/example/game_stash/VAddGameSearch.java
+++ app/src/main/java/com/example/game_stash/VAddGameSearch.java
@@ -2,7 +2,9 @@ package com.example.game_stash;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class VAddGameSearch extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class VAddGameSearch extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_game_search);
+    }
+
+    public void onClickGoToAddGameSearch(View view){
+        Intent intent = new Intent(this, VAddGameSearch.class);
+        startActivity(intent);
     }
 }
