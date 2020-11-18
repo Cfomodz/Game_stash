3f141f72a779dd3a6e0c07274112a6a5d2e632a5
David <david.ashby.lds@gmail.com>
2020-11-17 19:40:46 -0700

    Adding design and button functions

--- app/src/main/java/com/example/game_stash/VMenu.java
+++ app/src/main/java/com/example/game_stash/VMenu.java
@@ -24,7 +24,7 @@ public class VMenu extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_menu);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
