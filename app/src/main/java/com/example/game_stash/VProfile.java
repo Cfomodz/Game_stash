1bf98240304c79d595bb42d00fc9bfe26a9abf54
man18008 <meganmanwaring@gbyui.edu>
2020-11-17 18:21:19 -0700

    profile: added upload and back up buttons, including onclick events

--- app/src/main/java/com/example/game_stash/VProfile.java
+++ app/src/main/java/com/example/game_stash/VProfile.java
@@ -2,7 +2,9 @@ package com.example.game_stash;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class VProfile extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class VProfile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+    }
+
+    public void upload(View view) {
+        Intent intent = new Intent(this, VAddGameManually.class);
+        startActivity(intent);
+    }
+
+    public void backup(View view) {
+        Intent intent = new Intent(this, VAddGameManually.class);
+        startActivity(intent);
     }
 }
