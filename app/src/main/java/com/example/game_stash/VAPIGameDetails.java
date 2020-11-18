ca3f74c7800aa4173a40c81d639806292125f29a
blbaileyiii <blbaileyiii.naa@gmail.com>
2020-11-18 05:44:47 -0800

    basic updates

--- app/src/main/java/com/example/game_stash/VAPIGameDetails.java
+++ app/src/main/java/com/example/game_stash/VAPIGameDetails.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class VapiGameDetails extends AppCompatActivity {
+public class VAPIGameDetails extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
