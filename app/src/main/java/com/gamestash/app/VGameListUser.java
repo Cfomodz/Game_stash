b002512bdb18f3294270eda2d8bbe38567d1af25
David <david.ashby.lds@gmail.com>
2020-12-09 17:37:19 -0700

    Finished Filter Child Layout (popup) Added Spinner Texts for Filter popup Added HTML for email sending (Wish List)

--- app/src/main/java/com/gamestash/app/VGameListUser.java
+++ app/src/main/java/com/gamestash/app/VGameListUser.java
@@ -7,6 +7,8 @@ import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -16,14 +18,12 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import org.w3c.dom.Text;
-
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class VGameListUser extends AppCompatActivity {
+public class VGameListUser extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     private static final String TAG = VGameListUser.class.getSimpleName();
     private PGameListUser presenter;
     private List<DGame> gameList;
@@ -107,6 +107,7 @@ public class VGameListUser extends AppCompatActivity {
         adapter.notifyDataSetChanged();
     }
 
+
     public void onClickFilter(View view){
         TextView txtClose;
         Spinner spinnerOne;
@@ -119,8 +120,178 @@ public class VGameListUser extends AppCompatActivity {
                 myDialog.dismiss();
             }
         });
+        initializeViews();
         myDialog.show();
     }
 
+    public void initializeViews () {
+        Spinner spinnerOne = myDialog.findViewById(R.id.spinnerOne);
 
+        ArrayAdapter<CharSequence> adapterOne = ArrayAdapter.createFromResource(this, R.array.categories, R.layout.filter_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        //Toast.makeText(this, "test", Toast.LENGTH_SHORT).show();
+
+        spinnerOne.setAdapter(adapterOne);
+        updateSpinnerTwo();
+
+        spinnerOne.setOnItemSelectedListener(this);
+
+
+    }
+
+    public void updateSpinnerTwo(){
+        Spinner spinnerTwo = myDialog.findViewById(R.id.spinnerTwo);
+
+        ArrayAdapter<CharSequence> adapterTwo = ArrayAdapter.createFromResource(this, R.array.spinnerTwo, R.layout.filter_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinnerTwo.setAdapter(adapterTwo);
+        spinnerTwo.setOnItemSelectedListener(this);
+
+        //adapter.notifyDataSetChanged();
+
+    }
+
+    public void filterGames(String text){
+
+        if(text.equals("No Of Players")){
+            Toast.makeText(this, "Filter by: No of Players", Toast.LENGTH_SHORT).show();
+            //call to filter games by no of players
+            Log.d(TAG, filterGamesByNoPlayers().toString());
+            //filterGamesByNoPlayers();
+        }
+        if(text.equals("Play Time")){
+            Toast.makeText(this, "Filter by: Play Time:", Toast.LENGTH_SHORT).show();
+            //call to filter games by amount of time
+            Log.d(TAG, filterGamesByTime().toString());
+            //filterGamesByTime();
+        }
+        if(text.equals("Locations")){
+            Toast.makeText(this, "Filter by: Locations", Toast.LENGTH_SHORT).show();
+            //call to filter games by Location
+            Log.d(TAG, filterGamesByLocation().toString());
+            //filterGamesByLocation();
+        }
+        if(text.equals("Wish List")){
+            Toast.makeText(this, "Filter by: Wish List", Toast.LENGTH_SHORT).show();
+            //call to filter games by WishList
+            Log.d(TAG, filterGamesByWishList().toString());
+            //filterGamesByWishList();
+        }
+
+    }
+
+    public List<Integer> filterGamesByNoPlayers() {
+
+        List<DGame> gameList = DApp.getUserGameList().getGameList();
+        List<Integer> gamesPlayers = new ArrayList<>();
+
+        for (DGame game : gameList) {
+            int min = 0;
+            int max = 0;
+            if (game.getMinPlayers() > 0) {
+                min = game.getMinPlayers();
+            }
+            if (game.getMaxPlayers() > 0) {
+                max = game.getMaxPlayers();
+            }
+            for (int i = min; i <= max; i++) {
+                gamesPlayers.add(i);
+            }
+        }
+
+        //Log.d(TAG, gamesPlayers.toString());
+
+        //Make gamesPlayers Unique
+        List<Integer> gamesPlayersWithoutDuplicates = new ArrayList<>(
+                new HashSet<>(gamesPlayers));
+
+        //this is the final results that should be shown in the second spinner
+        //Log.d(TAG, gamesPlayersWithoutDuplicates.toString());
+        return gamesPlayersWithoutDuplicates;
+    }
+
+    public List<Integer> filterGamesByTime() {
+        List<DGame> gameList = DApp.getUserGameList().getGameList();
+        List<Integer> gamesTime = new ArrayList<>();
+
+        for (DGame game : gameList) {
+            int min = 0;
+            int max = 0;
+            if (game.getMinPlayTime() > 0) {
+                min = game.getMinPlayTime();
+            }
+            if (game.getMaxPlayTime() > 0) {
+                max = game.getMaxPlayTime();
+            }
+            for (int i = min; i <= max; i++) {
+                gamesTime.add(i);
+            }
+        }
+
+        //Log.d(TAG, gamesTime.toString());
+
+        //Make gamesTime Unique
+        List<Integer> gamesTimeWithoutDuplicates = new ArrayList<>(
+                new HashSet<>(gamesTime));
+
+        //Log.d(TAG, gamesTimeWithoutDuplicates.toString());
+
+        List<Integer> gamesTimeOptions = new ArrayList<>();
+        for (Integer i : gamesTimeWithoutDuplicates) {
+            if (i > 4) {
+                if (i % 5 == 0) {
+                    gamesTimeOptions.add(i);
+                }
+            }else{
+                gamesTimeOptions.add(i);
+            }
+        }
+
+        //this is the final results that should be shown in the second spinner
+        //Log.d(TAG, gamesTimeOptions.toString());
+        return gamesTimeOptions;
+    }
+
+    public List<String> filterGamesByWishList(){
+        List<String> wishListOptions = new ArrayList<>();
+        wishListOptions.add("Show");
+        wishListOptions.add("Hide");
+
+        //this is the final results that should be shown in the second spinner
+        //Log.d(TAG, wishListOptions.toString());
+        return wishListOptions;
+    }
+
+    public List<String> filterGamesByLocation(){
+        List<DGame> gameList = DApp.getUserGameList().getGameList();
+        List<String> listWithDuplicates =  new ArrayList<>();
+        Log.d(TAG,listWithDuplicates.toString());
+        listWithDuplicates.add("Wish List");
+        for (DGame game: gameList){
+            if(game.getLocation().trim().length()>0) {
+                listWithDuplicates.add(game.getLocation());
+            }
+        }
+        List<String> listWithoutDuplicates = listWithDuplicates.stream()
+                .distinct()
+                .collect(Collectors.toList());
+
+        //this is the final results that should be shown in the second spinner
+        //Log.d(TAG,listWithoutDuplicates.toString());
+        return listWithoutDuplicates;
+    }
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        filterGames(text);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
 }
--- app/src/main/java/com/gamestash/app/VSettings.java
+++ app/src/main/java/com/gamestash/app/VSettings.java
@@ -35,23 +35,57 @@ public class VSettings extends AppCompatActivity {
     public void onClickShareWishList(View view) {
 
         List<DGame> gameList = DApp.getUserGameList().getGameList();
-        List<String> listOfGames =  new ArrayList<>();
+        String outputHTML = "";
+        String htmlOpener = "<!DOCTYPE html><html><body>";
+        String htmlHeader = "<h2>Game Wish List</h2>";
+
+        outputHTML = outputHTML.concat(htmlOpener);
+        outputHTML = outputHTML.concat(htmlHeader);
+
+        Log.d(TAG,outputHTML);
+
         for (DGame game: gameList){
-            listOfGames.add(game.getGameName());
+            String htmlTableOpen = "<table style=\"width:100%\"";
+
+            String imageURL = game.getImageURL();
+            String gameName = game.getGameName();
+
+            String publisher = game.getPublisher().toString();
+
+            String minPlayers = game.getMinPlayers().toString();
+            String maxPlayers = game.getMaxPlayers().toString();
+
+            String minAge = game.getMinAge().toString();
+            minAge = minAge.concat("+");
+
+            String description = game.getDescription();
+
+            String htmlTableHeading = "<tr><th><img src=\""+imageURL+"\"></th><th>" + gameName +"</th>";
+            String htmlTableRow = "<tr><td>Publisher: "+publisher+"</td></tr>";
+            String htmlTableRow2 = "<tr><td>Players: " + minPlayers + " - " + maxPlayers + "</td><td>Age: " + minAge + "</td></tr>";
+            String htmlTableRow3 = "<tr><td>" + description + "</td></tr>";
+            String htmlTableClose = "</table>";
+
+            String gameString = htmlTableOpen + htmlTableHeading + htmlTableRow + htmlTableRow2 + htmlTableRow3 + htmlTableClose;
+
+            outputHTML = outputHTML.concat(gameString);
         }
 
-        Log.d(TAG,listOfGames.toString());
+        String htmlClose = "</body></html>";
+        outputHTML = outputHTML.concat(htmlClose);
 
+        Log.d(TAG,outputHTML);
+
+        /*
         String subject = "My Game Wish List";
-        String body = listOfGames.toString();
 
         EditText email = findViewById(R.id.etTo);
 
         Intent emailIntent = new Intent(Intent.ACTION_SEND);
         emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{email.getText().toString()});
         emailIntent.putExtra(Intent.EXTRA_SUBJECT, subject);
-        emailIntent.putExtra(Intent.EXTRA_TEXT, body);
-        //emailIntent.putExtra(Intent.EXTRA_HTML_TEXT, body); //If you are using HTML in your body text
+        //emailIntent.putExtra(Intent.EXTRA_TEXT, body);
+        emailIntent.putExtra(Intent.EXTRA_HTML_TEXT, outputHTML); //If you are using HTML in your body text
         emailIntent.setType("message/rfc822");
         //emailIntent.setData(Uri.parse("mailto:"));
         if(emailIntent.resolveActivity(getPackageManager()) != null){
@@ -59,5 +93,7 @@ public class VSettings extends AppCompatActivity {
         }else{
             Toast.makeText(VSettings.this,"There is no email application installed", Toast.LENGTH_SHORT).show();
         }
+
+         */
     }
 }
